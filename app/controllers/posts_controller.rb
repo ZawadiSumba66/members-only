7e06aa3601b4c180493a23740eaa5e6003c5a69e
SamArsanios <samson.kibrom@hotmail.com>
2021-02-22 13:37:28 +0300

    Add Posts Controller

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,26 @@
+class PostsController < ApplicationController
+    
+    def index
+        @posts = Post.all
+    end
+
+    def new
+        @post = current_user.posts.build
+    end
+
+    def create
+        @post = current_user.posts.build(post_params)
+    
+        if @post.save
+            redirect_to root_path
+        else
+            render :new
+        end
+    end
+
+    private
+
+    def post_params
+        params.require(:post).permit(:title, :body)
+    end
+end
